024305af16590c09ba2e5c6b61ed6866ff845f2c
vietnh1295 <vietnh1295@gmail.com>
2019-06-17 09:29:49 +0700

    add

---
+++ src/SuDungLopArrayIndexOutOfBoundsException.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SuDungLopArrayIndexOutOfBoundsException {
+    public static int[] createRandom() {
+         Random rd = new Random();
+        int[] arr = new int[100];
+        System.out.println("Danh sách phần tử của mảng: " );
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i]+ " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = createRandom();
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scaner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
